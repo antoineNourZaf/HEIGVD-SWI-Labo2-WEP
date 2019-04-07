d984ced5b338ae3e8481acea95b5d6bcc5a87449
Nevan92 <nevan92@localhost.localdomain>
2019-04-07 20:38:13 +0200

    modifs

--- files/manual-encryption.py
+++ files/manual-encryption.py
@@ -4,35 +4,36 @@
 from scapy.all import *
 import rc4
 import zlib
-
-# Donnees à chiffrer
-plaintext = "test de chiffrement"
-
-# IV de 24 bits
-iv = 0x123
-
-# Cle wep de 5 bytes AA:AA:AA:AA:AA
-key = '\xaa\xaa\xaa\xaa\xaa'
-seed = iv + key
+import binascii
 
 # Debut de l'algorithme
 if __name__ == '__main__':
 
-    # L'IV et la cle sont passés dans RC4 pour obtenir la keystream
-    keystream = rc4.rc4crypt(seed, key)
+    # Donnees à chiffrer
+    plaintext = "test de chiffrement avec message long"
+
+    # Cle wep de 5 bytes AA:AA:AA:AA:AA
+    key = '\xaa\xaa\xaa\xaa\xaa'
+
+    # Trame de départ pour avoir le template
+    arp = rdpcap('arp.cap')[0]
+
+    # La seed est composé de IV + clé
+    seed = arp.iv + key
 
     # Calcul de l'ICV
-    icv = zlib.crc32(plaintext)
-    icv = bytes([icv])
+    icv = binascii.crc32(plaintext)
+    icv_clair = struct.pack('<i', icv)
 
     # on concatene l'icv et le plaintext
-    plaintext = bytes(plaintext, "utf-8")
-    tmp = b''.join(plaintext,icv)
+    plaintext = plaintext + icv_clair
 
-    # XOR du keystream et du plain et de l'ICV
-    ciphertext = (tmp != keystream)
+    # Chiffrement par RC4
+    ciphertext = rc4.rc4crypt(plaintext, seed)
 
-    packet = b''.join(iv, ciphertext)
+    # Prerpare les datas
+    arp.wepdata = ciphertext[:-4]
+    (arp.icv,) = struct.unpack('!L', ciphertext[-4:])
 
     # Write packet to a pcap file
-    wrpcap('wepEncrypted.cap', packet)
+    wrpcap('wepEncrypted.cap', arp)
